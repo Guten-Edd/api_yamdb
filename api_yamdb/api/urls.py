83a7fe382013f22180545b3cfeddab12dfc3cc2a
Эдуард Соловьев <eddsolovei@gmail.com>
2023-02-04 21:21:18 +0300

    creating model "comment" + api for it + fixing bugs in review branch.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, GenreViewSet, ReviewViewSet
+from .views import CategoryViewSet, GenreViewSet, ReviewViewSet, CommentViewSet
 
 app_name = 'api'
 
@@ -23,7 +23,11 @@ router_v1.register(
     CategoryViewSet,
     basename='categories'
 )
-
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
